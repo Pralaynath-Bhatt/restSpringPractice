ae86f17153cb9afe782d5ff329a8202428076680
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-06-29 17:51:58 +0530

    using bcrypt in db to store passwords

--- 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/securityConfig.java
+++ 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/securityConfig.java
@@ -1,40 +1,24 @@
 package com.luv2code.springboot.cruddemo.security;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class securityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails raj = User.builder()
-                .username("raj")
-                .password("{noop}raj")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails rakshi = User.builder()
-                .username("rakshi")
-                .password("{noop}rakshi")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-
-        UserDetails sonia = User.builder()
-                .username("sonia")
-                .password("{noop}sonia")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(raj,rakshi,sonia);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -49,4 +33,24 @@ public class securityConfig {
         http.csrf(csrf-> csrf.disable());
         return http.build();
     }
+//    public InMemoryUserDetailsManager userDetailsManager({
+////        UserDetails raj = User.builder()
+////                .username("raj")
+////                .password("{noop}raj")
+////                .roles("EMPLOYEE")
+////                .build();
+////
+////        UserDetails rakshi = User.builder()
+////                .username("rakshi")
+////                .password("{noop}rakshi")
+////                .roles("EMPLOYEE","MANAGER")
+////                .build();
+////
+////        UserDetails sonia = User.builder()
+////                .username("sonia")
+////                .password("{noop}sonia")
+////                .roles("EMPLOYEE","MANAGER","ADMIN")
+////                .build();
+////        return new InMemoryUserDetailsManager(raj,rakshi,sonia);
+////    }
 }
