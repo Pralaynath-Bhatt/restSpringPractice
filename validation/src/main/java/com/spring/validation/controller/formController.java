711be53a86c325807f9c3aaca5b73da69c521c9a
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-07-02 23:55:39 +0530

    validatation on integer input

--- validation/src/main/java/com/spring/validation/controller/formController.java
+++ validation/src/main/java/com/spring/validation/controller/formController.java
@@ -2,15 +2,25 @@ package com.spring.validation.controller;
 
 import com.spring.validation.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class formController {
+
+    @InitBinder
+    public void preProcessor(WebDataBinder webDataBinder){
+        StringTrimmerEditor stringTrimmerEditor= new StringTrimmerEditor(true);
+
+        webDataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
 
     @GetMapping("/form")
     public String getForm(Model theModel){
--- validation/src/main/java/com/spring/validation/model/Customer.java
+++ validation/src/main/java/com/spring/validation/model/Customer.java
@@ -1,5 +1,7 @@
 package com.spring.validation.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,9 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+    @Min(value = 0,message = "Must be Greater than or equal to 0")
+    @Max(value = 10, message = "Must be less than or equal to 10")
+    private int freePass;
 
     public String getFirstName() {
         return firstName;
@@ -16,6 +21,14 @@ public class Customer {
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public int getFreePass() {
+        return freePass;
+    }
+
+    public void setFreePass(int freePass) {
+        this.freePass = freePass;
     }
 
     public String getLastName() {
