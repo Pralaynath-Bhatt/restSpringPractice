e504fa6eda0b16dedc54a9b1b59a36a71f4b66ef
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-07-11 15:11:29 +0530

    sequrity completed by bcrypt

--- demosecurity/src/main/java/com/springtutorial/demosecurity/security/DemoSecurity.java
+++ demosecurity/src/main/java/com/springtutorial/demosecurity/security/DemoSecurity.java
@@ -6,29 +6,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurity {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails raj = User.builder()
-                .username("raj")
-                .password("{noop}raj")
-                .roles("EMPLOYEE").build();
-        UserDetails raskhi = User.builder()
-                .username("rakshi")
-                .password("{noop}rakshi")
-                .roles("EMPLOYEE","MANAGER").build();
-        UserDetails sonia = User.builder()
-                .username("sonia")
-                .password("{noop}sonia")
-                .roles("EMPLOYEE","MANAGER","ADMIN").build();
-        return new InMemoryUserDetailsManager(raj,raskhi,sonia);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
+
+        return  jdbcUserDetailsManager;
     }
-
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
     http.authorizeHttpRequests(configerer->configerer
@@ -46,7 +45,6 @@ public class DemoSecurity {
             )
             .exceptionHandling(configerer->configerer
                     .accessDeniedPage("/access-Denied"));
-    ;
     return http.build();
     }
 }
