1ff3887181651d96a3fff233d58ca72969b75952
Pralaynath-Bhatt <uniquecuber04@gmail.com>
2025-07-16 22:19:03 +0530

    aop Before

--- aop/src/main/java/com/spring/aop/AopApplication.java
+++ aop/src/main/java/com/spring/aop/AopApplication.java
@@ -1,7 +1,10 @@
 package com.spring.aop;
 
+import com.spring.aop.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopApplication {
@@ -9,5 +12,16 @@ public class AopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AopApplication.class, args);
 	}
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO accountDAO){
+		return runner ->{
+			addAccountDemo(accountDAO);
+		};
+	}
 
+	private void addAccountDemo(AccountDAO accountDAO) {
+		System.out.println(getClass() + " Inside Main");
+		accountDAO.addAccount();
+
+	}
 }
---
+++ aop/src/main/java/com/spring/aop/dao/AccountDAO.java
@@ -0,0 +1,7 @@
+package com.spring.aop.dao;
+
+public interface AccountDAO {
+
+    void addAccount();
+
+}
